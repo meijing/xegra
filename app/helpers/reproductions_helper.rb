3616a2c1ec081f456f04f4177e811bc8d2349475
proxectos <proxectos@caminopaula@gmail.com>
2012-10-30 00:34:47 +0100

    añadido cuadro de reproduccion-vaca

--- app/helpers/reproductions_helper.rb
+++ app/helpers/reproductions_helper.rb
@@ -1,9 +1,20 @@
 module ReproductionsHelper
   def get_simbol_reproduction(reproductions, cow_id)
     info_repro = Array.new(12, Hash.new)
-    #content_tag :td, "sasd"
-    reproductions.each do |t|
-      t.cow_id
+    reproductions.each do |r|
+      if (r.cow_id == cow_id)
+        if (r.reproduction_simbol_id.nil?)
+          info_repro[r.month-1] = "".concat(r.comment)
+        else
+          info_repro[r.month-1] = ReproductionSimbol.find_by_id(r.reproduction_simbol_id).simbol.concat(" "+r.comment)
+        end
+      end
     end
+    return info_repro
+  end
+
+  def proba
+    a=""
+    a+=content_tag(:h1,"kk")
   end
 end
