8a628537b6a1c74fd1e7211fa52c172f9df328f5
proxectos <proxectos@caminopaula@gmail.com>
2012-12-26 17:10:26 +0100

    listados

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Xegra::Application.routes.draw do
   resources :reproduction_simbols
   scope "/:locale", :constraints => {:locale => /es|gl/} do
     delete '/facturation_milks', :to => 'facturation_milks#destroy'
+    match 'reports' =>'reports#index', :as =>'reports'
     match 'kine/is_not_pregnant' =>'kine#set_is_not_pregnant', :as =>'is_not_pregnant'
     match 'kine/is_pregnant' =>'kine#set_is_pregnant', :as =>'is_pregnant'
     match 'kine/notifications' =>'kine#notifications', :as =>'notifications'
