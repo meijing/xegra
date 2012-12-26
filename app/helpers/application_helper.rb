a6aedd82327ad20440ee55c0ff1d0c1c60185787
Eneko Taberna <eneko.taberna@gmail.com>
2012-12-26 17:08:52 +0100

    Refactorizar cows y controlers, mejora vista errores

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def error_messages_for models
+     if @cow.errors.any?
+        content_tag :div, id: "error_explanation"
+
+        t('activerecord.errors.title')
+
+        %ul
+        - @cow.errors.full_messages.each do |msg|
+        %li= msg
+
+      end
 end
